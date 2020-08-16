56cfd11f111058489b49a97845cbf6d045d99a17
Aman Zishan M.A <amanzishan.az@gmail.com>
2020-08-16 21:51:48 +0530

    Create pig_latin.py

---
+++ pig_latin.py
@@ -0,0 +1,14 @@
+#Let's create a function that turns text into pig latin: a simple text transformation that modifies each word moving the first character to the end and appending "ay" to the end. For example, python ends up as ythonpay.
+
+def pig_latin(text):
+  say = ""
+  # Separate the text into words
+  words = text.split()
+  for word in words:
+    # Create the pig latin word and add it to the list
+    say = say+word[1:len(word)]+"ay "
+    # Turn the list back into a phrase
+  return say
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
